7c96ad9b32ded1afa76b18a88e2962858b9838a8
Stuart P. Bentley <stuart@testtrack4.com>
2013-11-15 02:50:20 -0500

    Add replacement logic (sans capitalization tricks)

--- keyboardleopard.js
+++ keyboardleopard.js
@@ -1,85 +1,78 @@
-// ==UserScript==
-// @name s/keyboard/leopard/g
-// @version 3.1
-// @description Replaces the word "keyboard" with "leopard".
-// @match *://*/*
-// @license License In Three Lines
-// @updateURL http://userscripts.org/scripts/source/128626.meta.js
-// @downloadURL https://userscripts.org/scripts/source/128626.user.js
-// ==/UserScript==
-
 // Copyright 2013 Stuart P. Bentley.
 // This work may be used freely as long as this notice is included.
 // The work is provided "as is" without warranty, express or implied.
 
-// The pattern to match
-var keyboard_pattern = /(k)(e)(y)(b)o(ard)/ig
-
-// which letters in "keyboard" get replaced with which in "leopard"
-var leopard_subs = {
-  'k': 'l', 'K': 'L',
-  'y': 'o', 'Y': 'O',
-  'b': 'p', 'B': 'P'
-};
-
-// construct "leopard" replacement for "keyboard" components
-function replacement_leopard(match,k,e,y,b,ard){
-    return leopard_subs[k] + e +  leopard_subs[y] + leopard_subs[b] + ard
+function escapeRegExp(str) {
+  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
 }
 
-// Transform all instances of 'keyboard' in a string into 'leopard'
-function leopardize(str) {
-  return str.replace(keyboard_pattern, replacement_leopard)
+var filters;
+var allFilterRegexp;
+
+function loadFilters(){
+  filters = localStorage.getItem('filters');
+  allFilterRegexp = new RegExp('('+
+    filters.keys.map(escapeRegExp).join('|') + ')','gi');
+}
+
+function replacement(match) {
+  //TODO: alter case to match
+  return filters[match.toLowerCase()].outword;
+}
+
+function performSubstitutions(str) {
+  return str.replace(allFilterRegexp, replacement);
 }
 
 // define the noteType value for TEXT_NODEs (the kind we want to replace)
 // Some browsers may fail to make this value available - it's 3
-var TEXT_NODE = Node.TEXT_NODE || 3
+var TEXT_NODE = Node.TEXT_NODE || 3;
 
 // Flag to signal that we're replacing text, so that change doesn't trigger
 // another replacement (technically, that can't happen if all the instances
 // of "keyboard" that would trigger a replacement have been replaced with
 // "leopard", but it's still good practice)
-var replacingContent = false
+var replacingContent = false;
 
 function replaceTextContent(node) {
-  if (~node.textContent.search(keyboard_pattern)) {
+  if (~node.textContent.search(allFilterRegexp)) {
     //flag that content is being replaced so the event it generates
     //won't trigger another replacement
-    replacingContent = true
-    node.textContent = leopardize(node.textContent)
-    replacingContent = false
+    replacingContent = true;
+    node.textContent = performSubstitutions(node.textContent);
+    replacingContent = false;
   }
 }
 
 function changeTextNodes(node) {
-  var length, childNodes
-  //If this is a text node, leopardize it
+  var length, childNodes;
+  //If this is a text node, attempt substitutions on it
   if (node.nodeType == TEXT_NODE) {
-    replaceTextContent(node)
+    replaceTextContent(node);
   //If this is anything other than a text node, recurse any children
   } else {
-    childNodes = node.childNodes
-    length = childNodes.length
-    for(var i=0; i<length; ++i){
-      changeTextNodes(childNodes[i])
+    childNodes = node.childNodes;
+    length = childNodes.length;
+    for(var i = 0; i < length; ++i){
+      changeTextNodes(childNodes[i]);
     }
   }
 }
 
 function insertion_listener(event) {
   //change any new text nodes in a node that is added to the body
-  changeTextNodes(event.target)
+  changeTextNodes(event.target);
 }
 
 function cdm_listener(event) {
   //avoid infinite loop by ignoring events triggered by replacement
   if(!replacingContent){
-    replaceTextContent(event.target)
+    replaceTextContent(event.target);
   }
 }
 
-changeTextNodes(document.body)
-document.title = leopardize(document.title)
-document.body.addEventListener ("DOMNodeInserted", insertion_listener, false)
-document.body.addEventListener ("DOMCharacterDataModified", cdm_listener, false)
+loadFilters();
+changeTextNodes(document.body);
+document.title = performSubstitutions(document.title);
+document.body.addEventListener("DOMNodeInserted", insertion_listener, false);
+document.body.addEventListener("DOMCharacterDataModified", cdm_listener, false);
