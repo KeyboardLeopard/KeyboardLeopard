5714bf203809221026fb5067018ee4bae6af2052
Stuart P. Bentley <stuart@testtrack4.com>
2013-11-28 07:08:15 -0500

    Initialize Active checkbox on popup

--- popup.js
+++ popup.js
@@ -1,6 +1,11 @@
 /*global chrome*/
+
 document.getElementById('optionslink').addEventListener('click',
   function(){chrome.tabs.create({url: "options.html"});});
 
 document.getElementById('active').addEventListener('click',
   function(evt){chrome.storage.sync.set({active: evt.target.checked});});
+
+chrome.storage.sync.get({active: true}, function(res){
+  document.getElementById('active').checked = res.active;
+});
