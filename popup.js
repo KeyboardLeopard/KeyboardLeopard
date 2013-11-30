caa012621f20abbf1a05e8f7348dd2118e3585f8
Stuart P. Bentley <stuart@testtrack4.com>
2013-11-30 02:32:51 -0500

    Add OK and Reload buttons to popup

--- popup.js
+++ popup.js
@@ -1,10 +1,22 @@
 /*global chrome*/
 
 document.getElementById('optionslink').addEventListener('click',
-  function(){chrome.tabs.create({url: "options.html"});});
+  function(evt){chrome.tabs.create({url: "options.html"});});
 
 document.getElementById('active').addEventListener('click',
   function(evt){chrome.storage.sync.set({active: evt.target.checked});});
+
+function reloadTab() {
+  chrome.tabs.reload();
+}
+
+document.getElementById('reload-button').addEventListener('click', reloadTab);
+
+function dismissPopup(){
+  window.close();
+}
+
+document.getElementById('ok-button').addEventListener('click', dismissPopup);
 
 chrome.storage.sync.get({active: true}, function(res){
   document.getElementById('active').checked = res.active;
